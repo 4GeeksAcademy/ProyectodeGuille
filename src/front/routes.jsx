14cbd8727445765c63cfaf3443a121d1dd95fa8b
NatiSen <ola.eufaloportugues@gmail.com>
2025-11-14 21:28:37 +0000

    Resolved conflicts and updated develop

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -4,18 +4,27 @@ import {
   Route,
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Forgot } from "./pages/Forgot";
 import { Reset } from "./pages/Reset"; 
 import { MapView } from "./pages/MapView";
-
-
-
+import { Single } from "./pages/Single";
+import { Demo } from "./pages/Demo";
 import User from "./pages/User";
+
+const BASE_NAME = import.meta.env.VITE_BASENAME || "/";
+
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />   
+      <Route path="/single/:theId" element={<Single />} />
+      <Route path="/demo" element={<Demo />} />
       <Route index element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
@@ -25,5 +34,13 @@ export const router = createBrowserRouter(
       <Route path="*" element={<Login />} />
       <Route path="/user" element={<User />} />
     </Route>
-  )
+  ),
+    {
+        basename: BASE_NAME
+    }
 );
+
+  
+
+
+
